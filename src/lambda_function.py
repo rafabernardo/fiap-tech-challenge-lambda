ab9033599f1d527f8a597634d90481fb852e03b0
Alceu Ramos <alceu.rcjr@gmail.com>
2024-12-15 21:07:44 -0300

    feat: Mock function response

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,56 +1,62 @@
-import json
-import re
-
-import requests
-
-
-def validate_cpf(cpf):
-    return bool(re.match(r"^\d{11}$", cpf))
-
-
-def call_mock_endpoint(cpf):
-    url = "https://jsonplaceholder.typicode.com/posts"  # Mock endpoint
-    payload = {"cpf": cpf}
-    headers = {"Content-Type": "application/json"}
-
-    response = requests.post(url, data=json.dumps(payload), headers=headers)
-    return response
-
-
 def lambda_handler(event, context):
-    try:
-        body = json.loads(event.get("body", "{}"))
-        cpf = body.get("cpf", "")
+    return {
+        'statusCode': 200,
+        'body': 'Hello from Lambda!'
+    }
 
-        if not validate_cpf(cpf):
-            return {
-                "statusCode": 400,
-                "body": json.dumps(
-                    {"error": "CPF inválido. Deve conter 11 dígitos numéricos."}
-                ),
-            }
+# import json
+# import re
 
-        response = call_mock_endpoint(cpf)
+# import requests
 
-        if response.status_code == 201:
-            return {
-                "statusCode": 200,
-                "body": json.dumps(
-                    {
-                        "message": "Mock endpoint chamado com sucesso.",
-                        "data": response.json(),
-                    }
-                ),
-            }
-        else:
-            return {
-                "statusCode": 500,
-                "body": json.dumps({"error": "Falha ao chamar o mock endpoint."}),
-            }
-    except Exception as e:
-        return {
-            "statusCode": 500,
-            "body": json.dumps(
-                {"error": "Erro interno no servidor.", "details": str(e)}
-            ),
-        }
+
+# def validate_cpf(cpf):
+#     return bool(re.match(r"^\d{11}$", cpf))
+
+
+# def call_mock_endpoint(cpf):
+#     url = "https://jsonplaceholder.typicode.com/posts"  # Mock endpoint
+#     payload = {"cpf": cpf}
+#     headers = {"Content-Type": "application/json"}
+
+#     response = requests.post(url, data=json.dumps(payload), headers=headers)
+#     return response
+
+
+# def lambda_handler(event, context):
+#     try:
+#         body = json.loads(event.get("body", "{}"))
+#         cpf = body.get("cpf", "")
+
+#         if not validate_cpf(cpf):
+#             return {
+#                 "statusCode": 400,
+#                 "body": json.dumps(
+#                     {"error": "CPF inválido. Deve conter 11 dígitos numéricos."}
+#                 ),
+#             }
+
+#         response = call_mock_endpoint(cpf)
+
+#         if response.status_code == 201:
+#             return {
+#                 "statusCode": 200,
+#                 "body": json.dumps(
+#                     {
+#                         "message": "Mock endpoint chamado com sucesso.",
+#                         "data": response.json(),
+#                     }
+#                 ),
+#             }
+#         else:
+#             return {
+#                 "statusCode": 500,
+#                 "body": json.dumps({"error": "Falha ao chamar o mock endpoint."}),
+#             }
+#     except Exception as e:
+#         return {
+#             "statusCode": 500,
+#             "body": json.dumps(
+#                 {"error": "Erro interno no servidor.", "details": str(e)}
+#             ),
+#         }
